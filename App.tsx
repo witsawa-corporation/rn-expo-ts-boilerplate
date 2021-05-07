149e7a464eaf466310e688e8884e89ba135afba0
ak1103dev <ak.apichan1103@gmail.com>
2021-05-07 20:49:05 +0700

    :sparkles: react native global props

--- App.tsx
+++ App.tsx
@@ -2,8 +2,16 @@ import { StatusBar } from 'expo-status-bar'
 import { ThemeProvider } from '@emotion/react'
 import React from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { setCustomTextInput, setCustomText } from 'react-native-global-props'
 import Navigation from './src/navigations'
 import theme from './src/theme'
+
+setCustomText({
+  allowFontScaling: false,
+})
+setCustomTextInput({
+  allowFontScaling: false,
+})
 
 export default function App(): JSX.Element {
   return (
